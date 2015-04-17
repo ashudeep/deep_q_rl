688b4e3884d8caf1dca4f72c8d971bb6e8e82b80
Ashudeep Singh <ashudeep21@gmail.com>
2015-04-18 04:47:44 +0530

    initial commit for compilation of results

--- deep_q_rl/ale_results_recompile.py
+++ deep_q_rl/ale_results_recompile.py
@@ -4,32 +4,39 @@ visualization turned off.
 Usage:
 
 ale_run_watch.py NETWORK_PKL_FILE [ ROM ]
-
+"""
+'''
 __author__ = "Ashudeep Singh"
 __email__ = "ashudeep21@gmail.com"
-"""
-import subprocess
-import sys
+'''
 
-def main():
-	parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-r', '--rom', dest="rom", default=DEFAULT_ROM,
-                        help='ROM to run (default: %(default)s)')
-    parser.add_argument('--nn_file', type=str, default=None,
-                            help='Pickle file containing trained net.')
-    parameters, unknown = parser.parse_known_args(args)
-    run_test(parameters)
+import subprocess
+import sys, argparse
+
+DEFAULT_ROM = 'breakout'
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('-r', '--rom', dest="rom", default=DEFAULT_ROM,
+	help='ROM to run (default: %(default)s)')
+parser.add_argument('--nn_file', type=str, default=None,
+	help='Pickle file containing trained net.')
+
+def main(args):
+	parameters, unknown = parser.parse_known_args(args)
+	run_test(parameters)
 
 def run_test(parameters):
-    command = ['./ale_run.py', '--glue-port', '4097', '--steps-per-epoch', '0',
+	if parameters.nn_file == None:
+		print "Please provide the NETWORK_PKL_FILE"
+		parser.print_help()
+		exit(1)
+	command = ['./ale_run.py', '--glue-port', '4097', '--steps-per-epoch', '0',
                '--test-length', '10000', '--nn_file', parameters.nn_file]
 
-    if len(sys.argv) > 2:
-        command.extend(['--rom', parameters.rom])
-
-    p1 = subprocess.Popen(command)
+	command.extend(['--rom', parameters.rom])
+	print command
+	p1 = subprocess.Popen(command)
     
-    p1.wait()
+	p1.wait()
 
 if __name__ == "__main__":
-    main()
+	main(sys.argv[1:])
